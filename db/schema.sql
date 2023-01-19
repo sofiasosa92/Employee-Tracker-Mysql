acd63ac8736363119ea21b669dbf3415a138bf8c
Sofia92 <sofiasosaperez@outlook.com>
2023-01-18 18:47:24 -0600

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT 
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (name)
+VALUES ("Sofia"), ("Damien"), ("Ren");
+
+INSERT INTO role
+(title,salary),
